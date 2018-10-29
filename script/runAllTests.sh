e1c13f1a6e27bc8f9d0f589452ee2699822990c7
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-10-29 08:06:01 +0530

    Change directory structure.
    move patternLibTest file to test and create a new directory script
    which contains the runtest and run all test.

---
+++ script/runAllTests.sh
@@ -0,0 +1,3 @@
+./run_test.sh ../createDiamond.js ../appTestData/input/diamondInput ../appTestData/output/diamondOutput ;
+./run_test.sh ../createTriangle.js ../appTestData/input/triangleInput ../appTestData/output/triangleOutput ;
+./run_test.sh ../createRectangle.js ../appTestData/input/rectangleInput ../appTestData/output/rectangleOutput; 
