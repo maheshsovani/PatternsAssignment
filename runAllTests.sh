075880f2e7752e4d89d3eb9215956de9bc40c1ca
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-10-29 08:09:57 +0530

    stage deleted files

--- runAllTests.sh
+++
@@ -1,3 +0,0 @@
-./testScripts/run_test.sh ./createDiamond.js appTestData/input/diamondInput appTestData/output/diamondOutput ;
-./testScripts/run_test.sh ./createTriangle.js appTestData/input/triangleInput appTestData/output/triangleOutput ;
-./testScripts/run_test.sh ./createRectangle.js appTestData/input/rectangleInput appTestData/output/rectangleOutput; 
